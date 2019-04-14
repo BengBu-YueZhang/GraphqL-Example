e156b294d0165a921caee317158fc9233125596f
BengBu-YueZhang <zhangyue_coder@163.com>
2019-04-14 14:42:16 +0800

    reset

--- server/config/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  port: 3000,
-  mongoServer: process.env.NODE_ENV === 'production' ? '' : 'mongodb://localhost/test2',
-  NODE_ENV: process.env.NODE_ENV === 'production' ? 'production' : 'development'
-}
