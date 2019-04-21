16154e2cd1d6f993c4c7fad2e3729c189c36a160
BengBu-YueZhang <zhangyue_coder@163.com>
2019-04-21 21:56:26 +0800

    cors

--- server/app.js
+++ server/app.js
@@ -3,14 +3,29 @@ const json = require('koa-json')
 const bodyparser = require('koa-bodyparser')
 const config = require('./config')
 const port = config.URL.port
+const cors = require('@koa/cors')
 
 const app = new Koa()
 const UserRoute = require('./routes/user.router')
 
 config.mongo.mongoConnect()
 
+app.use(cors({
+  origin: '*',
+  credentials: true,
+  methods: ['PUT', 'POST', 'GET', 'DELETE', 'OPTIONS'],
+  allowedHeaders: [
+    'Content-Type',
+    'Content-Length',
+    'Authorization',
+    'Accept',
+    'X-Requested-With',
+    'x-access-token'
+  ]
+}))
 app.use(UserRoute.routes(), UserRoute.allowedMethods())
 app.use(bodyparser())
 app.use(json())
+
 
 module.exports = app.listen(port)
