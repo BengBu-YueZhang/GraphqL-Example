8992ea80c8f87bca82a2264602b7ae4d8c235d86
BengBu-YueZhang <zhangyue_coder@163.com>
2019-04-21 17:30:55 +0800

    init server

---
+++ server/app.js
@@ -0,0 +1,50 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const app = new Koa()
+const router = new Router()
+
+const co = require('co')
+const convert = require('koa-convert')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const debug = require('debug')('koa2:server')
+const path = require('path')
+
+const config = require('./config')
+const routes = require('./routes')
+
+const port = process.env.PORT || config.port
+
+onerror(app)
+
+app.use(bodyparser())
+  .use(json())
+  .use(logger())
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - $ms`)
+})
+
+router.get('/', async (ctx, next) => {
+  ctx.state = {
+    title: 'Koa2'
+  }
+  await ctx.render('index', ctx.state)
+})
+
+routes(router)
+app.on('error', function(err, ctx) {
+  console.log(err)
+  logger.error('server error', err, ctx)
+})
+
+module.exports = app.listen(config.port, () => {
+  console.log(`Listening on http://localhost:${config.port}`)
+})
