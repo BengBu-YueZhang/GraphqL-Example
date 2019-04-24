006b8e2d0551cac0e97082cb9b6f5e5089ae0109
BengBu-YueZhang <zhangyue_coder@163.com>
2019-04-24 22:26:52 +0800

    add getUsers controller

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,3 +1,5 @@
+const UserModel = require('../models/user.model')
+
 module.exports = {
   async login (ctx, next) {
   },
@@ -6,6 +8,17 @@ module.exports = {
   },
 
   async getUsers (ctx, next) {
+    try {
+      const data = await UserModel.find()
+      ctx.result = {
+        data,
+        msg: 'success',
+        code: 200
+      }
+      await next()
+    } catch (error) {
+      throw error
+    }
   },
 
   async getUserById (ctx, next) {
