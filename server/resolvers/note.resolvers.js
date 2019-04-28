bf7b9e3ace4986e49a1922a68c207cd6c3ade7ab
BengBu-YueZhang <zhangyue_coder@163.com>
2019-04-28 17:10:00 +0800

    update

--- server/resolvers/note.resolvers.js
+++ server/resolvers/note.resolvers.js
@@ -4,7 +4,7 @@ module.exports = {
     },
     note () {
     },
-    me () {
+    meNote () {
     }
   },
   Mutation: {
--- server/schemas/note.schema.js
+++ server/schemas/note.schema.js
@@ -4,7 +4,7 @@ const typeDefs = gql`
   type Query {
     notes(pagestart: Int = 1, pagesize: Int = 10): NotesResponse!
     note(id: ID!): NoteResponse
-    me: NoteResponse
+    meNote: NoteResponse
   }
 
   type Mutation {
