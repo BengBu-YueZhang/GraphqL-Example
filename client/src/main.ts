7963f65d87fddfc41f4db9cd4254962d65dccc61
BengBu-YueZhang <zhangyue_coder@163.com>
2019-04-29 00:18:43 +0800

    update

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import './public/style/reset.less';
+import './public/style/variable.less';
+import './public/style/common.less';
 
 Vue.config.productionTip = false;
 
