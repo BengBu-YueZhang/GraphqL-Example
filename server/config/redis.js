89b5b2be335addf04300ee676f7c80dfabe3ac6d
BengBu-YueZhang <zhangyue_coder@163.com>
2019-05-02 00:05:05 +0800

    update

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,6 +1,10 @@
 const redis = require("redis")
 const client = redis.createClient()
 
+client.auth('root', () => {
+  console.log('redis验证成功')
+})
+
 client.on("error", (err) => {
   console.log('Error ' + err)
 })
