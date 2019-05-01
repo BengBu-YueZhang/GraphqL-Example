8ee52a5beb53d22ebdb0463b762aaaf421a14e62
BengBu-YueZhang <zhangyue_coder@163.com>
2019-05-02 00:13:24 +0800

    update

--- client/src/util/http.ts
+++ client/src/util/http.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { isHaveStorage, getLocalStorage, removeLocalStorage } from '@/util/storage';
 
 const NODE_ENV: string = process.env.NODE_ENV;
-const baseURL = NODE_ENV === 'development' ? 'http://127.0.0.1:5000/' : 'http://gqldemo.bengbuzhangyue.xyz';
+const baseURL = NODE_ENV === 'development' ? 'http://127.0.0.1:5000/' : 'http://apigql.bengbuzhangyue.xyz';
 
 const Axios = axios.create({
   baseURL,
