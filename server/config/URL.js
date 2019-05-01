a05d7247f18db73b9c34766c2a4740429caab66b
BengBu-YueZhang <zhangyue_coder@163.com>
2019-05-01 23:05:44 +0800

    update

--- server/config/URL.js
+++ server/config/URL.js
@@ -8,7 +8,7 @@ const config = {
   },
   production: {
     port: 10010,
-    mongo: `mongodb://${`gql`}:${12345678}@${10010}:27017/gqldeom`,
+    mongo: `mongodb://${`gql`}:${12345678}@127.0.0.1:27017/gqldeom`,
     graphql: '/graphql'
   }
 }
